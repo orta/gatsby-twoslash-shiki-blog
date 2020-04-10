af2015bebb19f0dad544f28c3488ec1bebd6666f
Orta Therox <orta.therox@gmail.com>
2020-04-10 09:16:27 -0400

    Customize

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `gatsby-remark-shiki-twoslash Example Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Orta Therox`,
+      summary: `Engineer.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `A starter blog demonstrating gatsby-remark-shiki-twoslash.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `orta`,
     },
   },
   plugins: [
